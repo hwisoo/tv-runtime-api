6f93638bdb7fd3ab898cefd77614eb910fee2d68
James Cho and Leilani Leach <james@>
2019-01-16 11:50:04 -0800

    empty list of results when submitting new search

--- src/main.js
+++ src/main.js
@@ -3,22 +3,27 @@ import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+
 $(document).ready(function () {
 
   $("#userForm").submit(function (event) {
     event.preventDefault();
     let minutes = $('#minutes').val();
     let genre = $('select#genre').val();
-
+    $("#name").empty();
+    $("#id").empty();
 
 
     $.ajax({
-      url: `https://api.themoviedb.org/3/discover/tv?api_key=APIKEY&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=${genre}&with_runtime.lte=${minutes}&include_null_first_air_dates=false`,
+      url: `https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=${genre}&with_runtime.lte=${minutes}&include_null_first_air_dates=false`,
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function (response) {
+        if (response.results.length == 0) {
+          $('#errors').text("There was an error processing your request. Please try again.");
+        }
         for (let i = 0; i < response.results.length; i++) {
           $("#name").append("<li>" + response.results[i].name + '</li>');
           $("#id").append("<li>" + response.results[i].id + '</li>');
