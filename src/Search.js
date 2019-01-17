cc79eca96dfd8f1737c4d986fa625594da55d346
James Cho and Leilani Leach <leilani@>
2019-01-17 13:05:47 -0800

    refactor to use arrow notation

--- src/Search.js
+++ src/Search.js
@@ -5,13 +5,12 @@ export class SearchMovie {
     this.runtime = runtime;
     this.genre = genre;
     this.results = [];
-   
+
   }
   GetNameList() {
-    let that = this;
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=${that.genre}&with_runtime.lte=${that.runtime}&include_null_first_air_dates=false`;
+      let url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=${this.genre}&with_runtime.lte=${this.runtime}&include_null_first_air_dates=false`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
@@ -25,7 +24,7 @@ export class SearchMovie {
   }
 
   GetDetails(id) {
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
       let url = `https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}&language=en-US`;
       request.onload = function () {
@@ -39,5 +38,5 @@ export class SearchMovie {
       request.send();
     });
   }
-  
+
 }
