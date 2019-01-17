da84c0052f74fec24a2f316b004d636962c3154e
James Cho and Leilani Leach <james@>
2019-01-17 10:43:14 -0800

    add image thumbnails to show details

--- src/main.js
+++ src/main.js
@@ -24,25 +24,29 @@ $(document).ready(function () {
       for (let i = 0; i < body.results.length; i++) {
         $("#name").append(`<li class= ${body.results[i].id}>${body.results[i].name}</li>`);
       }
-      $("li").click(function(event){
+      $("li").click(function (event) {
         $("#details").empty();
         $("#details").show();
         let id = this.getAttribute("class");
         let detailsPromise = newSearch.GetDetails(id);
-        detailsPromise.then(function(response){
+        detailsPromise.then(function (response) {
           let body = JSON.parse(response);
+
+
+          $("#details").append(`<img class='img-thumbnail' src='https://image.tmdb.org/t/p/original/${body.backdrop_path}' alt = 'pic'>`);
           $("#details").append(`<h4>${body.name}</h4>`);
           $("#details").append(`<p>Number of seasons: ${body.number_of_seasons}</p>`);
           $("#details").append(`<p>Number of episodes: ${body.number_of_episodes}</p>`);
           $("#details").append(`<p id='description'>Description: ${body.overview}</p>`);
           $("#details").append(`<p>Episode runtime: ${body.episode_run_time[0]}</p>`);
 
+
         })
-        
+
       })
-    }) 
- 
+    })
+
 
   })
-  
+
 })
