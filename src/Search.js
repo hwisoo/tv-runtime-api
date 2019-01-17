38d8508577e7c1138e8b2eea65e662588543a502
James Cho and Leilani Leach <james@>
2019-01-17 09:16:55 -0800

    add second API call to display details when TV show clicked

--- src/Search.js
+++ src/Search.js
@@ -5,6 +5,7 @@ export class SearchMovie {
     this.runtime = runtime;
     this.genre = genre;
     this.results = [];
+   
   }
   GetNameList() {
     let that = this;
@@ -23,9 +24,23 @@ export class SearchMovie {
     });
   }
 
-  // GetDetails(id) {
-
-  // }
+  GetDetails(id) {
+    // let that = this;
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.themoviedb.org/3/tv/${id}?api_key=${API_KEY}&language=en-US`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+  
 }
   //   })
   //   $.get(`https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=${this.genre}&with_runtime.lte=${this.runtime}&include_null_first_air_dates=false`)
